c1701666d5ecc5d6152709f6906c074d26bce189
Fiido <roccobosio@gmail.com>
2024-03-06 11:14:18 -0300

    websockets ok

---
+++ backend/src/chat/chat.gateway.ts
@@ -0,0 +1,49 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { RoomI, joinUser, leaveUser } from 'src/utils/user';
+
+@WebSocketGateway()
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  rooms: RoomI[] = [
+    { id: 'default', usersOn: [] },
+    { id: 'cine', usersOn: [] },
+    { id: 'juegos', usersOn: [] },
+  ];
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log('client connected', client.id);
+  }
+
+  handleDisconnect(client: any) {
+    console.log('client disconnected', client.id);
+  }
+
+  @SubscribeMessage('join')
+  join(client: Socket, dto: { roomId: string; userName: string }): string {
+    client.join(dto.roomId);
+    this.server.emit('users', JSON.stringify(joinUser(this.rooms, dto)));
+    return `joined to room: ${dto.roomId}`;
+  }
+
+  @SubscribeMessage('leave')
+  leave(client: Socket, dto: { roomId: string; userName: string }): string {
+    client.leave(dto.roomId);
+    this.server.emit('users', JSON.stringify(leaveUser(this.rooms, dto)));
+    return `leaved to room: ${dto.roomId}`;
+  }
+
+  @SubscribeMessage('send')
+  test(client: Socket, msg: { roomId: string; message: string }): string {
+    this.server.to(msg.roomId).emit('message', msg.message);
+    return msg.message;
+  }
+}
---
+++ backend/src/utils/user.ts
@@ -0,0 +1,39 @@
+export interface RoomI {
+  id: string;
+  usersOn: string[];
+}
+
+export const joinUser = (
+  rooms: RoomI[],
+  dto: { roomId: string; userName: string },
+) => {
+  const room: RoomI = rooms.find((room: RoomI) => room.id === dto.roomId);
+
+  if (!room) return rooms;
+
+  room.usersOn.push(dto.userName);
+
+  const idx: number = rooms.findIndex((room: RoomI) => room.id === dto.roomId);
+  rooms[idx] = room;
+
+  return rooms;
+};
+
+export const leaveUser = (
+  rooms: RoomI[],
+  dto: { roomId: string; userName: string },
+) => {
+  const room: RoomI = rooms.find((room: RoomI) => room.id === dto.roomId);
+
+  if (!room) return rooms;
+
+  const idx: number = rooms.findIndex((room: RoomI) => room.id === dto.roomId);
+
+  const roomIdx = room.usersOn.findIndex((user) => user === dto.userName);
+  if (roomIdx === -1) return rooms;
+  room.usersOn.splice(roomIdx, 1);
+
+  rooms[idx] = room;
+
+  return rooms;
+};
